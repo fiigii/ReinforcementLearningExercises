36a74105fa10a0b12465866d9373fc4351fea6f9
Fei Peng <feipeng.compiler@gmail.com>
2023-04-12 01:48:18 -0700

    Solving mountain car by Q-learning with discretization

---
+++ MountainCar/Discretization.py
@@ -0,0 +1,206 @@
+import sys
+import gymnasium as gym
+import numpy as np
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+env = gym.make('MountainCar-v0', render_mode="human")
+
+""" 
+state = env.reset()
+score = 0
+for t in range(200):
+    action = env.action_space.sample()
+    env.render()
+    state, reward, done, _, _= env.step(action)
+    score += reward
+    if done:
+        break 
+print('Final score:', score)
+env.close()
+"""
+
+def make_grid_linspace(low, high, bins):
+    return [np.linspace(low[dim], high[dim], bins[dim] + 1)[1:-1] for dim in range(len(bins))]
+
+def make_grid_scratch(low, high, bins):
+    res = []
+    for (l, h, b) in zip(low, high, bins):
+        local_res = []
+        interval = (h - l) / b
+        for i in range(b + 1):
+            local_res.append(l + i * interval)
+        res.append(local_res[1:-1])
+    return res
+
+def make_grid_comprehension(low, high, bins):
+    res = [ [l + (h-l)/b * j for j in range(1,b)] for i,(l,h,b) in enumerate(zip(low,high,bins)) ]
+    return res
+
+
+def create_uniform_grid(low, high, bins=(10, 10)):
+    """Define a uniformly-spaced grid that can be used to discretize a space.
+    
+    Parameters
+    ----------
+    low : array_like
+        Lower bounds for each dimension of the continuous space.
+    high : array_like
+        Upper bounds for each dimension of the continuous space.
+    bins : tuple
+        Number of bins along each corresponding dimension.
+    
+    Returns
+    -------
+    grid : list of array_like
+        A list of arrays containing split points for each dimension.
+    """
+    grid = make_grid_linspace(low, high, bins)
+    print("Uniform grid: [<low>, <high>] / <bins> => <splits>")
+    for l, h, b, splits in zip(low, high, bins, grid):
+        print("    [{}, {}] / {} => {}".format(l, h, b, splits))
+    return grid
+
+def discretize(sample, grid):
+    """Discretize a sample into the given grid.
+    
+    Parameters
+    ----------
+    sample : array_like
+        A list of continuous values to be discretized.
+    grid : list of array_like
+        A list of arrays containing split points for each dimension.
+    
+    Returns
+    -------
+    discretized_sample : array_like
+        The discretized sample in the form of a list of indices.
+    """
+    return list(np.digitize(s, g) for s, g in zip(sample, grid))
+
+"""
+low = [-1.0, -5.0]
+high = [1.0, 5.0]
+grid = create_uniform_grid(low, high) 
+samples = np.array(
+    [[-1.0 , -5.0],
+     [-0.81, -4.1],
+     [-0.8 , -4.0],
+     [-0.5 ,  0.0],
+     [ 0.2 , -1.9],
+     [ 0.8 ,  4.0],
+     [ 0.81,  4.1],
+     [ 1.0 ,  5.0]])
+discretized_samples = np.array([discretize(sample, grid) for sample in samples])
+print("\nSamples:", repr(samples), sep="\n")
+print("\nDiscretized samples:", repr(discretized_samples), sep="\n")
+"""
+
+class QLearningAgent:
+    """Q-Learning agent that can act on a continuous state space by discretizing it."""
+
+    def __init__(self, env, state_grid, alpha=0.02, gamma=0.99,
+                 epsilon=1.0, epsilon_decay_rate=0.9995, min_epsilon=.01, seed=505):
+        """Initialize variables, create grid for discretization."""
+        # Environment info
+        self.env = env
+        self.state_grid = state_grid
+        self.state_size = tuple(len(splits) + 1 for splits in self.state_grid)  # n-dimensional state space
+        self.action_size = self.env.action_space.n  # 1-dimensional discrete action space
+        self.seed = np.random.seed(seed)
+        print("Environment:", self.env)
+        print("State space size:", self.state_size)
+        print("Action space size:", self.action_size)
+        
+        # Learning parameters
+        self.alpha = alpha  # learning rate
+        self.gamma = gamma  # discount factor
+        self.epsilon = self.initial_epsilon = epsilon  # initial exploration rate
+        self.epsilon_decay_rate = epsilon_decay_rate # how quickly should we decrease epsilon
+        self.min_epsilon = min_epsilon
+        
+        # Create Q-table
+        self.q_table = np.zeros(shape=(self.state_size + (self.action_size,)))
+        print("Q table size:", self.q_table.shape)
+
+    def preprocess_state(self, state):
+        """Map a continuous state to its discretized representation."""
+        return tuple(discretize(state, self.state_grid))
+
+    def reset_episode(self, state):
+        """Reset variables for a new episode."""
+        # Gradually decrease exploration rate
+        self.epsilon *= self.epsilon_decay_rate
+        self.epsilon = max(self.epsilon, self.min_epsilon)
+
+        # Decide initial action
+        self.last_state = self.preprocess_state(state)
+        self.last_action = np.argmax(self.q_table[self.last_state])
+        return self.last_action
+    
+    def reset_exploration(self, epsilon=None):
+        """Reset exploration rate used when training."""
+        self.epsilon = epsilon if epsilon is not None else self.initial_epsilon
+
+    def act(self, state, reward=None, done=None, mode='train'):
+        """Pick next action and update internal Q table (when mode != 'test')."""
+        state = self.preprocess_state(state)
+        if mode == 'test':
+            # Test mode: Simply produce an action
+            action = np.argmax(self.q_table[state])
+        else:
+            # Train mode (default): Update Q table, pick next action
+            # Note: We update the Q table entry for the *last* (state, action) pair with current state, reward
+            self.q_table[self.last_state + (self.last_action,)] += self.alpha * \
+                (reward + self.gamma * max(self.q_table[state]) - self.q_table[self.last_state + (self.last_action,)])
+
+            # Exploration vs. exploitation
+            do_exploration = np.random.uniform(0, 1) < self.epsilon
+            if do_exploration:
+                # Pick a random action
+                action = np.random.randint(0, self.action_size)
+            else:
+                # Pick the best action from Q table
+                action = np.argmax(self.q_table[state])
+
+        # Roll over current state, action for next step
+        self.last_state = state
+        self.last_action = action
+        return action
+
+def run(agent, env, num_episodes=20000, mode='train'):
+    """Run agent in given reinforcement learning environment and return scores."""
+    scores = []
+    max_avg_score = -np.inf
+    for i_episode in range(1, num_episodes+1):
+        # Initialize episode
+        state, _ = env.reset()
+        action = agent.reset_episode(state)
+        total_reward = 0
+        done = False
+
+        # Roll out steps until done
+        while not done:
+            state, reward, done, _, _ = env.step(action)
+            total_reward += reward
+            action = agent.act(state, reward, done, mode)
+
+        # Save final score
+        scores.append(total_reward)
+        
+        # Print episode stats
+        if mode == 'train':
+            if len(scores) > 100:
+                avg_score = np.mean(scores[-100:])
+                if avg_score > max_avg_score:
+                    max_avg_score = avg_score
+            if i_episode % 100 == 0:
+                print("\rEpisode {}/{} | Max Average Score: {}".format(i_episode, num_episodes, max_avg_score), end="")
+                sys.stdout.flush()
+
+    return scores
+
+state_grid = create_uniform_grid(env.observation_space.low, env.observation_space.high, bins=(10, 10))
+q_agent = QLearningAgent(env, state_grid)
+scores = run(q_agent, env)
--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Reinforcement Learning Exercises
 
-## Black Jack
+## OpenAI Gym
+### Black Jack
 Playing black jack by Monte Carlo method and optimize the policy by Monte Carlo Control.
 
-## Cliff Walking
-Solving cliff walking by 3 temporal difference methods.
+### Cliff Walking
+Solving cliff walking by 3 temporal difference methods using SARSA, Q-Learning, and expected SARSA.
 
-## Taxi Problem
+### Taxi Problem
 Description of the environment is in subsection 3.1 of [this paper](https://arxiv.org/pdf/cs/9905014.pdf).
+
+### Mountain Car
